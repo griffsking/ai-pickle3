c9162b793240896ae9c7ccc41e3db27fa48f6cdd
Firebase Studio <noreply@firebase.studio>
2025-07-23 22:53:44 +0000

    Initialized workspace with Firebase Studio

---
+++ src/bootstrap.js
@@ -0,0 +1,12 @@
+window["APP_TEMPLATE_BOOTSTRAP"] = {
+    geminiApiKey: "AIzaSyBpkIy5Dlg4LC2QOC1gCKQsq2HCT8NOmgI",
+
+    firebase: {
+      apiKey: "AIzaSyBToTul3CFDKt3Ip9TTuEzgL_5-syLefSM",
+      authDomain: "ai-pickle2.firebaseapp.com",
+      projectId: "ai-pickle2",
+      storageBucket: "ai-pickle2.firebasestorage.app",
+      messagingSenderId: "515733221066",
+      appId: "1:515733221066:web:0d9454d36fc5163b14ce33",
+    }
+    };
